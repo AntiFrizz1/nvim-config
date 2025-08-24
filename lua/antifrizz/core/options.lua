09e502f5cf88c25caeeace5e2964a4a5be62d340
AntiFrizz <gluhov1998@yandex.ru>
2025-08-24 19:58:03 +0300

    Fix searching path

--- lua/antifrizz/core/options.lua
+++ lua/antifrizz/core/options.lua
@@ -1,27 +1,29 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
-
 local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+-- encoding
+opt.fileencoding = "utf-8" -- the encoding written to a file
+opt.encoding = "utf-8" -- the encoding
 
--- tabs & indentation 
-opt.tabstop = 4       -- 4 spaces for tabs
-opt.shiftwidth = 4    -- 4 spaces for indent width
-opt.expandtab = true  -- expand tab to spaces
+-- tabs & indentation
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 opt.wrap = true -- display very long string on the next line
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you insert mixed case in search, assumes you want case-sensetive
+opt.smartcase = true -- if you insert mixed case in search, assumes you want case-sensetive
 
 -- turn on termguicolors
 opt.termguicolors = true
 opt.background = "dark" -- colorscheme that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -33,3 +35,5 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
+-- searching path
+opt.path = { ".", ",", "**" } -- "**" for recursive search in subdirs
